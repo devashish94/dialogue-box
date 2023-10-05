d999e6a66c0a036c4f6984bc0de1210bdf81f4cd
devashish94 <roy.devashish94@gmail.com>
2023-10-05 16:21:51 +0530

    change background color chat sender

--- app/src/ChatComponents/Chat.jsx
+++ app/src/ChatComponents/Chat.jsx
@@ -1,7 +1,7 @@
 export default function Chat({ chat, side }) {
   return (
     <>
-      <div className={`${side === 'left' ? 'self-start bg-neutral-600 rounded-r-lg rounded-tl-lg' : 'self-end bg-neutral-800 rounded-l-lg rounded-tr-lg'} text-sm px-4 py-2 flex flex-col space-y-1`}>
+      <div className={`${side === 'left' ? 'self-start bg-neutral-600 rounded-r-lg rounded-tl-lg' : 'self-end bg-neutral-700 rounded-l-lg rounded-tr-lg'} text-sm px-4 py-2 flex flex-col space-y-1`}>
         <p className="font-bold text-sm">{side === 'left' ? chat.username : ''}</p>
         <p>{chat.message} </p>
         <p className="self-end text-sm">{chat.time}</p>
