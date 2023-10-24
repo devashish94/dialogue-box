d3dfeeb5bb84ced9f5266a31a696ed9284ae4295
devashish94 <roy.devashish94@gmail.com>
2023-10-25 02:51:30 +0530

    add transition effect while opening emoji picker

--- app/src/ChatComponents/ChatComponent.jsx
+++ app/src/ChatComponents/ChatComponent.jsx
@@ -3,10 +3,11 @@ import Chat from "./Chat"
 // import Participants from "../Participants"
 
 export default function ChatComponent({ username, room, conversations, submitAction, sidebar, partSidebar }) {
+  const [emojiPicker, setEmojiPicker] = useState(false)
 
   return (
     <>
-      <div className=' flex flex-col h-full justify-between flex-grow rounded-md space-y-2 relative oveflow-hidden'>
+      <div className=' flex flex-col h-full justify-between flex-grow rounded-md space-y-2 relative oveflow-y-auto'>
         <nav className="flex w-full items-center justify-between space-x-6 px-3 py-1 shadow bg-gray-100 sm:bg-gray-50">
 
           <div className="flex space-x-2 items-center justify-between">
@@ -33,7 +34,8 @@ export default function ChatComponent({ username, room, conversations, submitAct
 
         </nav>
 
-        <div className='flex-grow rounded-md flex flex-col px-3 py-8 space-y-2 overflow-y-auto transition-all duration-500 ease-in-out bg-red-100'>
+        {/* <div className='flex-grow rounded-md flex flex-col px-3 py-8 space-y-2 overflow-y-auto transition-all duration-500 ease-in-out bg-red-100 hover:bg-red-200'> */}
+        <div className='flex-1 h-full rounded-md flex flex-col px-3 py-8 space-y-2 overflow-y-auto'>
           {
             conversations && conversations.map(function (conversation, index) {
               return (<Chat key={index} chat={conversation} side={conversation.username === username ? 'right' : 'left'} />)
@@ -41,11 +43,11 @@ export default function ChatComponent({ username, room, conversations, submitAct
           }
         </div>
 
-        <div className="h-20 w-full bg-black flex justify-center items-center relative hover:top-10 duration-1000 ease-in-out z-0">sample div</div>
+        <div className={`${emojiPicker ? 'h-80' : 'h-0'} w-full bg-black flex justify-center items-center transition-all duration-[250ms] ease-in-out text-white`}>Emoji Picker</div>
 
         {/* <form onSubmit={e => handle(e)} className='w-full flex items-center'> */}
         <form onSubmit={submitAction} className='w-full flex items-center space-x-3 px-1 py-2 shadow bg-gray-50 z-40'>
-          <button className="p-2 rounded-full">
+          <button onClick={() => setEmojiPicker(!emojiPicker)} className="p-2 rounded-full" type="button">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
               <path strokeLinecap="round" strokeLinejoin="round" d="M15.182 15.182a4.5 4.5 0 01-6.364 0M21 12a9 9 0 11-18 0 9 9 0 0118 0zM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75zm-.375 0h.008v.015h-.008V9.75zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75zm-.375 0h.008v.015h-.008V9.75z" />
             </svg>
@@ -57,6 +59,7 @@ export default function ChatComponent({ username, room, conversations, submitAct
             </svg>
           </button>
         </form>
+        {/* <div className={`${emojiPicker ? 'h-80' : 'h-0'} w-full bg-black flex justify-center items-center transition-all duration-[200ms] ease-in-out text-white`}>Emoji Picker</div> */}
       </div>
     </>
   )
